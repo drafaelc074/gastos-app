03df8564c2e257e76aa63ac388739301e4a8390b
Diego Rafael <drafaelc074@gmail.com>
2026-09-01 16:44:26 -0300

    test: adiciona teste de criacao de despesas

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -23,3 +23,26 @@ def test_home():
     data = response.json()
 
     assert data["status"] == "online"
+
+
+def test_criar_despesa():
+    nova_despesa = {
+        "descricao": "Internet",
+        "valor": 120.90,
+        "categoria": "Casa",
+        "data": "2026-09-01"
+    }
+
+    response = client.post(
+        "/despesas",
+        json=nova_despesa
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["descricao"] == "Internet"
+    assert data["valor"] == 120.90
+    assert data["categoria"] == "Casa"
+    assert "id" in data
